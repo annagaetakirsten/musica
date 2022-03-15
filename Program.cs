23a4b2c9f6e177aa1e77935e7a9d1b12275ce6c0
LAB-00\lab00-00 <e107ti@cps.sp.gov.br>
2022-03-15 13:24:46 -0300

    musica

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿# musica
+tarefa musica
+Console.WriteLine("Marilia Mendonça - BIN")
+Console.WriteLine("Esse é o hino dos moleque
+");
+Console.WriteLine("Sem amor com o bolso cheio de onça");
+Console.WriteLine("Como é que dança?");
+Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
+Console.WriteLine("Nossa vizinhança já já se cansa");
+Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
+
+Console.WriteLine("Sem amor com o bolso cheio de onça");
+Console.WriteLine("Como é que dança?");
+Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
+Console.WriteLine("Nossa vizinhança já já se cansa");
+Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
+
+Console.WriteLine("É que 'cê vale bem mais que isso");
+Console.WriteLine("Não preste ao papel");
+Console.WriteLine("Um pedacin' de Deus que eu roubei do céu");
+Console.WriteLine("Eu fiz cartas e quadros, eu gastei o pincel");
+Console.WriteLine("É que 'cê me deu motivo de usar o papel");
+
+Console.WriteLine("Amor, a vida vale o risco");
+Console.WriteLine("Tiro o meu chapéu");
+Console.WriteLine("Pra quem levou meu coração");
+Console.WriteLine("Não tem porque meu céu");
+Console.WriteLine("Mais bela do baile");
+Console.WriteLine("Eu merecia um troféu");
+Console.WriteLine("Por cair nessa ilusão");
+Console.WriteLine("Só com cachaça e mel");
+
+Console.WriteLine("Sem amor com o bolso cheio de onça");
+Console.WriteLine("Como é que dança?");
+Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
+Console.WriteLine("Nossa vizinhança já já se cansa");
+Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
+
+Console.WriteLine("Sem amor com o bolso cheio de onça");
+Console.WriteLine("Como é que dança?");
+Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
+Console.WriteLine("Nossa vizinhança já já se cansa");
+Console.WriteLine("Vou cantar sofrência igual Marília Mendonça (igual Marília Mendonça)");
+Console.WriteLine("Marília Mendonça");
+
+Console.ReadKey();
