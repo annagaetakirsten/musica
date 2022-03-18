3f6741eb0b0bc8e853e13d74ba205d1ded9e6e53
annagaetakirsten <anna.agkirsten@gmail.com>
2022-03-18 20:15:34 -0300

    tarefa realizada

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
-﻿# musica
-tarefa musica
-Console.WriteLine("Marilia Mendonça - BIN")
-Console.WriteLine("Esse é o hino dos moleque
-");
+﻿Console.ForegroundColor=
+System.ConsoleColor.Blue;
+Console.WriteLine("Marilia Mendonça - BIN");
+
+Console.ResetColor();
+Console.WriteLine("Esse é o hino dos moleque");
 Console.WriteLine("Sem amor com o bolso cheio de onça");
 Console.WriteLine("Como é que dança?");
 Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
 Console.WriteLine("Nossa vizinhança já já se cansa");
 Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
 
+Console.ForegroundColor=
+System.ConsoleColor.Blue;
 Console.WriteLine("Sem amor com o bolso cheio de onça");
 Console.WriteLine("Como é que dança?");
 Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
 Console.WriteLine("Nossa vizinhança já já se cansa");
 Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
 
+Console.ResetColor();
 Console.WriteLine("É que 'cê vale bem mais que isso");
 Console.WriteLine("Não preste ao papel");
 Console.WriteLine("Um pedacin' de Deus que eu roubei do céu");
@@ -30,12 +34,17 @@ Console.WriteLine("Eu merecia um troféu");
 Console.WriteLine("Por cair nessa ilusão");
 Console.WriteLine("Só com cachaça e mel");
 
+Console.ForegroundColor=
+System.ConsoleColor.Blue;
 Console.WriteLine("Sem amor com o bolso cheio de onça");
 Console.WriteLine("Como é que dança?");
 Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
 Console.WriteLine("Nossa vizinhança já já se cansa");
 Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
 
+Console.ResetColor();
+Console.ForegroundColor=
+System.ConsoleColor.Blue;
 Console.WriteLine("Sem amor com o bolso cheio de onça");
 Console.WriteLine("Como é que dança?");
 Console.WriteLine("Vou cantar sofrência igual Marília Mendonça");
